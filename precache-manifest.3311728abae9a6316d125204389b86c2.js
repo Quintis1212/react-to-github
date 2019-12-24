c44bf3e8808158992d66bdfff5bf691ad29557cc
Quintis <mataleo1212@gmail.com>
2019-12-24 21:56:46 +0200

    Updates

---
+++ precache-manifest.3311728abae9a6316d125204389b86c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41bb859c6eb0b3c3ba1d8482f90ee48b",
+    "url": "/react-to-github/index.html"
+  },
+  {
+    "revision": "0d41ed6e2e3bb58a9f1f",
+    "url": "/react-to-github/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bfb2c6ee08aa9611cf1e",
+    "url": "/react-to-github/static/js/2.761d9c05.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-to-github/static/js/2.761d9c05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d41ed6e2e3bb58a9f1f",
+    "url": "/react-to-github/static/js/main.52eab0a0.chunk.js"
+  },
+  {
+    "revision": "9a4f2790ee89778a6305",
+    "url": "/react-to-github/static/js/runtime-main.9f41e9ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
